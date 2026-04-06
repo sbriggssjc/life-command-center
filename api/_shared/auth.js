ec7b6160d7eedb2652a28b1cbdab36a1e2c52b79
Scott Briggs <sbriggssjc@gmail.com>
2026-04-06 12:25:54 -0500

    Fix truncated handleCors function in auth.js

--- api/_shared/auth.js
+++ api/_shared/auth.js
@@ -415,4 +415,13 @@ export function visibilityFilter(user, workspaceId) {
  */
 export function handleCors(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHe
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Prefer, X-LCC-Key, X-LCC-User-Id, X-LCC-User-Email, X-LCC-Workspace'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return true;
+  }
+  return false;
+}
